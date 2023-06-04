c530a8c0c507c71983b16aabe27b8df27a30be8e
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-06-04 15:15:29 +0600

    Add files via upload

---
+++ If_else.py
@@ -0,0 +1,12 @@
+age = 17
+if(age<= 13):
+    print("Hello ")
+    print("You are a kid")
+elif(age<=18 and age>13):
+    print("You are a teenager")
+else:
+    print("You are an adult")
+
+char = 'B'
+if(char== 'B' or 'D'):
+    print("Bangladesh")
