4d0947715e2f47281f848e00592967c9ddf715b1
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-06-04 15:17:01 +0600

    Add files via upload

---
+++ WhileLoop.py
@@ -0,0 +1,11 @@
+i =1
+while i<=10:
+    print("I love Bangladesh")
+    i +=1
+
+while i<=100:
+    if(i%2==0):
+        print(str(i)+" is an even number") # string & int value cannot be concatanated. so int value must convert to the string
+    else:
+        print(str(i)+ " is an odd number")
+    i +=1
