c1fb75b8e2ddd81296353a0d9c93e4bbbc960014
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-06-04 15:12:36 +0600

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,29 @@
+print("Mahir Faisal")
+print('Hello world')
+print('Hello I\'m Mahir')
+print("Hello I'm Mahir")
+var1 = 24.56
+var2 = 23
+var3= 23
+print(var1+var2)
+print(var1+var2+var3)
+var4 = "Our country name is Bangladesh"
+print(var4)
+# this is single line comment
+'''This is
+ Multi Line
+ comment '''
+# use multiple variable at a time
+var6 = " My name is "
+var7 = " Mahir."
+var9 = "Twenty three & twenty three makes "
+var8 = var3+var2
+print(var6+var7)  # they must to be same data type
+#print(var7+var8) this will  not run because two different data type cannot be concatanate
+# so we have to convert the data type of var8 into string, thats why we use str() function
+result = str(var8)
+print(var9 + result)
+x,y,z = (4,57.87,"Ki obosta")  # another way of declaring variables
+print(x)
+print(y)
+print(z)
