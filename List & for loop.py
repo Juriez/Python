454c39ad79137e271ecab87c5187f69f1faa8836
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-06-04 15:15:51 +0600

    Add files via upload

---
+++ List & for loop.py
@@ -0,0 +1,14 @@
+data = [12, 34, 7, 90, 67]
+print(data)
+print(data[2])
+# to excess the value from the ending sight, index will start from -1
+print(data[-1]) #it will print 67
+print(data[-3]) # this will print 7
+print("After using for loop :")
+
+for x in data:
+    print(x)
+
+data2 = ["abc", "def", "ghi"] # it is a list. any type of data can be included
+for x in data2:
+    print(x)
